a1a9c9f0cdba4776ff767e967eaf80039b8a4292
aadya <aadya.vashishth@phoenixtech.consulting>
2022-04-06 17:22:28 +0530

    updates

--- src/app/dashboard/create-course/request-detail/request-detail.component.ts
+++ src/app/dashboard/create-course/request-detail/request-detail.component.ts
@@ -18,6 +18,7 @@ export class RequestDetailComponent implements OnInit {
   purchaseOrder:any;
   getUserrole: any;
   routegetdata: any;
+  imgUrl:any;
   trainingDurationHours:any;
   status: any;
   transfer_user_id: any;
@@ -234,6 +235,9 @@ export class RequestDetailComponent implements OnInit {
     this.purchaseOrder  = this.routegetdata.purchase_order.split('#')[0];
     console.log("this.purchaseOrder",this.purchaseOrder);
     }
+    if(this.routegetdata.resource){
+    this.imgUrl = `https://orange.mindscroll.info/public/public/${this.routegetdata.resource}`;
+    }
     console.log(arr1);
     this.objectarray = [];//arr1.split('• ')
     //console.log(arr1.split('• '))
@@ -251,7 +255,13 @@ export class RequestDetailComponent implements OnInit {
     let learner_guideline = this.routegetdata.learner_guideline;
     console.log(JSON.parse(learner_guideline));
     console.log("new learnerGuidelines",this.learnerGuidelines);
+   //this.getImageUrl();
 
+  }
+  getImageUrl(): void{
+    this.imgUrl = `https://orange.mindscroll.info/public/public/${this.routegetdata.resource}`;
+    console.log("this.imgUrl",this.imgUrl);
+    return this.imgUrl;
   }
   setrejectbutton(id:any) {
     this.courseService.courseHistory(id).subscribe((res: any) => {
