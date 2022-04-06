c0b68c5ae345fb546ee23f1f4e454435543e55b6
aadya <aadya.vashishth@phoenixtech.consulting>
2022-04-06 14:56:33 +0530

    new

--- src/app/dashboard/open-course-session/open-course-session.module.ts
+++ src/app/dashboard/open-course-session/open-course-session.module.ts
@@ -7,20 +7,11 @@ import { CreateSessionComponent } from './create-session/create-session.componen
 import { OpenRequestDetailComponent } from './open-request-detail/open-request-detail.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { NgxPaginationModule } from 'ngx-pagination';
-<<<<<<< HEAD:src/app/dashboard/open-course-session/open-course-session.module.ts
 import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [AllCoursesComponent, CreateSessionComponent, OpenRequestDetailComponent],
-=======
-import { CreateOpenCourseComponent } from './create-open-course/create-open-course.component';
-import { OpenRequestDetailComponent } from './open-request-detail/open-request-detail.component';
-
-
-@NgModule({
-  declarations: [ AllCoursesComponent, CreateOpenCourseComponent, OpenRequestDetailComponent],
->>>>>>> module2-open-course:src/app/dashboard/open-course/open-course.module.ts
   imports: [
     CommonModule,
     SharedModule,
