bddf947480e4f187e829ffca4a9a1864cbf23fbf
aadya-phoenix <70717291+aadya-phoenix@users.noreply.github.com>
2022-05-02 18:55:46 +0530

    commit

--- src/app/dashboard/open-course-session/create-session/create-session.component.ts
+++ src/app/dashboard/open-course-session/create-session/create-session.component.ts
@@ -122,7 +122,7 @@ export class CreateSessionComponent implements OnInit {
       external_vendor: new FormControl('', [Validators.required]),
       external_vendor_name:new FormControl(''),
       manager_approval: new FormControl(''),
-      training_cost: new FormControl(''),
+      training_cost: new FormControl('0.00'),
     });
   }
 
